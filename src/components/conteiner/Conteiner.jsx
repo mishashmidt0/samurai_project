e8c407c3c9ffa1e3234ddce0017033c9a9709b6a
Misha Shmidt <mishashmidt0@gmail.com>
2021-12-29 15:57:15 +0300

    feat: add container data

--- src/components/conteiner/Conteiner.jsx
+++ src/components/conteiner/Conteiner.jsx
@@ -1,4 +1,3 @@
-
 import Nav from '../navigation/Navigation';
 
 import Profile from '../profile/Profile';
@@ -7,26 +6,26 @@ import Messsages from '../messsages/Messsages';
 import NotFound from '../notfound/NotFound';
 
 import s from './Conteiner.module.css';
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 
 
 function Conteiner() {
-    return (
-        <div className={s.conteiner}>
-            <Nav />
-            <div className={s.conteiner}>
-                <Routes>
+	return (
+		<div className={s.conteiner}>
+			<Nav/>
+			<div className={s.conteiner}>
+				<Routes>
 
-                    <Route path='/profile' element={<Profile />} />
+					<Route path='/profile' element={<Profile/>}/>
 
-                    <Route path='/messages' element={<Messsages />} />
-                    <Route path='*' element={<NotFound />} />
+					<Route path='/messages' element={<Messsages/>}/>
+					<Route path='*' element={<NotFound/>}/>
 
-                </Routes>
-            </div>
-        </div>
+				</Routes>
+			</div>
+		</div>
 
-    )
+	)
 }
 
 export default Conteiner;
