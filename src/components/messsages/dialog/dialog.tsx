a39af4ac40b10dd3c1288626c821394990fbebd6
Misha Shmidt <mishashmidt0@gmail.com>
2022-01-21 23:25:43 +0300

    fix: dialog type

--- src/components/messsages/dialog/dialog.tsx
+++ src/components/messsages/dialog/dialog.tsx
@@ -6,12 +6,13 @@ import React from "react";
 type forPropsDialog = {
 	id: number,
 	name: string,
+	message?: string
 }
 
 export function Dialog ( props: forPropsDialog ) {
 	return (
 		<div className={ s.dialog }>
-			<img className={ s.avatar } src={`https://cspromogame.ru//storage/upload_images/avatars/391${props.id}.jpg`} alt="avaatar"/>
+			<img className={ s.avatar } src={ `https://cspromogame.ru//storage/upload_images/avatars/391${ props.id }.jpg` } alt="avaatar"/>
 			<Link to={ `/messages/${ props.id }` }>{ props.name }</Link>
 		</div>
 	)
